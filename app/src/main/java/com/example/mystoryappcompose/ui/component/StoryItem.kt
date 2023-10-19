d9401ee8af5580edf5fd654ca38d9cc33dd5d814
mansao01 <novrilukman15@gmail.com>
2023-10-19 18:10:43 +0700

    update story item ui

--- app/src/main/java/com/example/mystoryappcompose/ui/component/StoryItem.kt
+++ app/src/main/java/com/example/mystoryappcompose/ui/component/StoryItem.kt
@@ -1,8 +1,12 @@
 package com.example.mystoryappcompose.ui.component
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -13,16 +17,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.mystoryappcompose.R
 import com.example.mystoryappcompose.data.network.response.ListStoryItem
+import com.example.mystoryappcompose.utils.DateFormatter
+import java.util.TimeZone
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun StoryItem(
-    story:ListStoryItem,
+    story: ListStoryItem,
     modifier: Modifier = Modifier
 ) {
     Card(
@@ -31,39 +39,48 @@ fun StoryItem(
             .fillMaxWidth(),
         shape = MaterialTheme.shapes.large
     ) {
-        Row {
-            AsyncImage(
-                model = ImageRequest.Builder(context = LocalContext.current)
-                    .data(story.photoUrl)
-                    .crossfade(true)
-                    .build(),
-                contentDescription = null,
-                placeholder = painterResource(id = R.drawable.loading_img),
-                error = painterResource(id = R.drawable.ic_broken_image),
-                modifier = Modifier
-                    .size(68.dp)
-                    .align(Alignment.CenterVertically)
-            )
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(top = 4.dp)
+                .fillMaxSize()
+        ) {
             Column(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 4.dp)
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text(
-                    text = story.name,
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.padding(4.dp),
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                )
-                Text(
-                    text = story.description,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(bottom = 8.dp)
+                AsyncImage(
+                    model = ImageRequest.Builder(context = LocalContext.current)
+                        .data(story.photoUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = null,
+                    placeholder = painterResource(id = R.drawable.loading_img),
+                    error = painterResource(id = R.drawable.ic_broken_image),
+                    modifier = Modifier
+                        .size(224.dp)
+
                 )
             }
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = DateFormatter.formatDate(story.createdAt, TimeZone.getDefault().id),
+                fontStyle = FontStyle.Italic,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Text(
+                text = story.name,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(4.dp),
+            )
+            Text(
+                text = story.description,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
         }
     }
 }
